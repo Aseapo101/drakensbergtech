1e13b4d8c7ce81391e2b2d929ade4f0f55ae8c0f
Aseapo101 <techdrakensberg@gmail.com>
2023-10-06 17:41:24 +0200

    unit testing and param validation in the pdf and csv parser

--- formgenerator-service/src/test/java/za/co/emerge/formgenerator/controller/FormGeneratorControllerTest.java
+++ formgenerator-service/src/test/java/za/co/emerge/formgenerator/controller/FormGeneratorControllerTest.java
@@ -64,34 +64,6 @@ public class FormGeneratorControllerTest
 	}
 	
 	@Test
-	public void getGeneratedPdfFileList() throws Exception 
-	{
-	
-		PDFform pdfEntity1 = new PDFform();
-		
-		pdfEntity1.setDocument(new byte [2000]);
-		pdfEntity1.setDocumentName("test_document_1");
-		
-		LocalDateTime dateTime = LocalDateTime.now();
-		pdfEntity.setLocalDateTime(dateTime);
-		
-		List<PDFform> formList = List.of(pdfEntity,pdfEntity1);
-		
-		
-		List<GeneratedPdfFormFile> generatedFileList = List.of(new GeneratedPdfFormFile(pdfEntity.getDocumentName(),"default_url",pdfEntity.getDocument().length,"application/pdf",pdfEntity.getLocalDateTime())
-																,new GeneratedPdfFormFile(pdfEntity1.getDocumentName(),"default_url",pdfEntity1.getDocument().length,"application/pdf",pdfEntity1.getLocalDateTime())) ;
-		
-		//given(f.getDateTimeCreated()).willReturn(dateTime);
-		given(pdfFileViewService.getHistoricalPdfFiles()).willReturn(new java.util.HashSet<PDFform> (formList));
-		
-		
-		this.mvc.perform(get("/viewfiles")).andDo(print())
-				.andExpectAll(status().isOk(),
-				model().attribute("showfiles_flag", true),
-				model().attribute("files", generatedFileList));
-	}
-	
-	@Test
 	public void generatePdfFile() throws Exception 
 	{
 	 
